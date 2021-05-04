bf77a8a40d4fd18f3fbe9b006d6e65b2cee62c7c
jamiedishy <jamiedishy@gmail.com>
2021-05-03 23:05:18 -0400

    Component - added dialog info card

---
+++ src/Components/DialogueInfoCard.js
@@ -0,0 +1,16 @@
+const DialogueInfoCard = () => {
+    return (
+        <div className="d-flex align-items-start flex-column" style={{height: '100%'}}>
+            <div>
+              <p className="candor-bold">Coleman H / Glenn L</p>
+              <p className="h2">Why hasn't Canada developed their own COVID-19 vaccine?</p>
+              <p className="font-weight-light">7 responses</p>
+            </div>
+            <div className="mt-auto">
+              <img alt='Candor logo' src='C.svg'/>
+            </div>
+        </div>
+    )
+}
+
+export default DialogueInfoCard
